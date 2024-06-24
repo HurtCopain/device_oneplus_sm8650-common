d5a276e68c73e826b604dded76e718b3b7a82992
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-24 18:53:11 +0800

    BoardConfigCommon: use ro.hwui.use_vulkan=true
    https://android-review.googlesource.com/c/platform/frameworks/base/+/2819917
    https://android-developers.googleblog.com/2024/05/the-second-beta-of-android-15.html
    
    Change-Id: I1c0d61de52583b297c8c112517ab817567fe968e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,6 +86,9 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # Fingerprint
 TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 
+# Graphics
+TARGET_USES_VULKAN := true
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/oplus/vintf/device_framework_matrix.xml \
